cbacce46c461641f0f6b04e703e01b492e81ecaf
Jason <extremewinds@gmail.com>
2024-06-02 05:49:06 +0200

    add sample command line runner bean

--- src/main/java/io/github/inbrackets/coffeeshop/CoffeeShopApplication.java
+++ src/main/java/io/github/inbrackets/coffeeshop/CoffeeShopApplication.java
@@ -2,10 +2,12 @@ package io.github.inbrackets.coffeeshop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -15,8 +17,16 @@ public class CoffeeShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoffeeShopApplication.class, args);
-        log.info("Application started successfully!!");
+        log.info("Application started successfully!");
         log.info("Something changed");
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            String message = "The runner is up and running!";
+            log.info(message);
+        };
     }
 
 }
