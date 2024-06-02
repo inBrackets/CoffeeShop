c8f0563aebe3c762eb5b6b41f7a0af53cc97c49d
Jason <extremewinds@gmail.com>
2024-06-02 05:40:27 +0200

    add sample logging

--- src/main/java/io/github/inbrackets/coffeeshop/CoffeeShopApplication.java
+++ src/main/java/io/github/inbrackets/coffeeshop/CoffeeShopApplication.java
@@ -1,5 +1,7 @@
 package io.github.inbrackets.coffeeshop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,8 +11,12 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class CoffeeShopApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(CoffeeShopApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CoffeeShopApplication.class, args);
+        log.info("Application started successfully!!");
+        log.info("Something changed");
     }
 
 }
