a93a57811c01c622786c1ba591bae4be64444b04
Jason <extremewinds@gmail.com>
2024-06-12 18:59:50 +0200

    add initial swagger setup

--- src/main/java/io/github/inbrackets/coffeeshop/CoffeeController.java
+++ src/main/java/io/github/inbrackets/coffeeshop/CoffeeController.java
@@ -1,5 +1,11 @@
 package io.github.inbrackets.coffeeshop;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@Tag(name = "Tutorial", description = "Tutorial management APIs")
 @RequestMapping("/coffees")
 public class CoffeeController {
     
@@ -23,6 +30,13 @@ public class CoffeeController {
         return coffeeRepository.findAll();
     }
 
+    @Operation(summary = "Retrieve all Tutorials", tags = { "tutorials", "get", "filter" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = Coffee.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "204", description = "There are no Tutorials", content = {
+                    @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
         return coffeeRepository.findById(id);
