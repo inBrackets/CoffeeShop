a2ecb19336038e9ed7bebcd2f4f2e827d4133451
Jason <extremewinds@gmail.com>
2024-06-13 22:37:31 +0200

    update swagger docs

--- src/main/java/io/github/inbrackets/coffeeshop/CoffeeController.java
+++ src/main/java/io/github/inbrackets/coffeeshop/CoffeeController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +17,41 @@ import java.util.UUID;
 @Tag(name = "Tutorial", description = "Tutorial management APIs")
 @RequestMapping("/coffees")
 public class CoffeeController {
-    
+
     private final CoffeeRepository coffeeRepository;
 
     public CoffeeController(CoffeeRepository coffeeRepository) {
         this.coffeeRepository = coffeeRepository;
     }
 
+    @Operation(summary = "Get all available coffees", tags = {"get"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = Coffee.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "204", description = "There are no Coffees", content = {
+                    @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @GetMapping
     Iterable<Coffee> getCoffees() {
         return coffeeRepository.findAll();
     }
 
-    @Operation(summary = "Retrieve all Tutorials", tags = { "tutorials", "get", "filter" })
+    @Operation(summary = "Get a Coffee object by id", tags = {"get"})
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = {
-                    @Content(schema = @Schema(implementation = Coffee.class), mediaType = "application/json") }),
-            @ApiResponse(responseCode = "204", description = "There are no Tutorials", content = {
-                    @Content(schema = @Schema()) }),
-            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
+                    @Content(schema = @Schema(implementation = Coffee.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
         return coffeeRepository.findById(id);
     }
 
+    @Operation(summary = "Create a new Tutorial", tags = {"post"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = {
+                    @Content(schema = @Schema(implementation = Coffee.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @PostMapping
     ResponseEntity<Coffee> postCoffee(@RequestBody Coffee coffee) {
         if (coffee.getId() == null || coffee.getId().isEmpty()) {
@@ -50,6 +60,12 @@ public class CoffeeController {
         return new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a Coffee by Id", tags = {"tutorials", "put"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = Coffee.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema())})})
     @PutMapping("/{id}")
     ResponseEntity<Coffee> putCoffee(@PathVariable String id,
                                      @RequestBody Coffee coffee) {
@@ -59,6 +75,9 @@ public class CoffeeController {
                 : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete a coffee by a coffee id", tags = {"delete"})
+    @ApiResponses({@ApiResponse(responseCode = "204", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", content = {@Content(schema = @Schema())})})
     @DeleteMapping("/{id}")
     void deleteCoffee(@PathVariable String id) {
         coffeeRepository.deleteById(id);
